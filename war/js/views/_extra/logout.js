18393927db21495bf793f34fe40412ef09919c9d
Cengiz Ulusoy <culusoy@ask-cs.com>
2012-08-06 14:12:52 +0000

    commit from coda environment

---
+++ war/js/views/_extra/logout.js
@@ -0,0 +1,20 @@
+$("document").ready(function()
+{
+	localStorage.clear();
+/*
+	webpaige.con(
+		options = {
+			path: '/logout',
+			loading: 'Logging out..'
+			,session: session.getSession()	
+		},
+		function(data)
+	  {
+	  	var login = webpaige.get('login');  
+			localStorage.clear();
+			localStorage.set('login', login);
+			//document.location = "login.html";
+		}
+	); 
+*/
+});
