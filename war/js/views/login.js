28f1b4768edbb846f33f3df7dc870743095d442f
Cengiz Ulusoy <culusoy@ask-cs.com>
2012-11-20 11:35:25 +0100

    post-maitenance / pre-changes in code

--- war/js/views/login.js
+++ war/js/views/login.js
@@ -1,38 +1,35 @@
-$(document).ready(function()
+$(document).ready(function ()
 {
-
- 	pageInit('login', 'false');
- 	var login = JSON.parse(webpaige.get('login'));
- 	if (login != null)
- 	{
-	  $('#username').val(login.user);
-	  $('#remember').attr('checked', login.remember);
- 	}
-  $("#alertClose").click(function()
+  pageInit('login', 'false');
+  var login = JSON.parse(webpaige.get('login'));
+  if (login != null)
+  {
+    $('#username').val(login.user);
+    $('#remember').attr('checked', login.remember);
+  }
+  $("#alertClose").click(function ()
   {
     $("#alertDiv").hide();
   });
-  $("#loginBtn").click(function()
+  $("#loginBtn").click(function ()
   {
-  	loginHandler();
+    loginHandler();
   });
-	window.addEventListener( 'keypress', KeyPressHandler );
-
-
-	var local = {
-		title: 'login_title',
-		statics: ['login_login', 'login_enter_email', 'login_password', 'login_remember', 'login_forgot']		
-	}
-	webpaige.i18n(local);
+  window.addEventListener('keypress', KeyPressHandler);
+  var local = {
+    title: 'login_title',
+    statics: ['login_login', 'login_enter_email', 'login_password', 'login_remember', 'login_forgot']
+  }
+  webpaige.i18n(local);
 });
 
-
-function KeyPressHandler( event ) {
-	if ( event.keyCode === 13 ) {
-		loginHandler();
-	}
+function KeyPressHandler(event)
+{
+  if (event.keyCode === 13)
+  {
+    loginHandler();
+  }
 }
-
 
 function loginHandler()
 {
@@ -45,7 +42,7 @@ function loginHandler()
   if (user == '' || user == userDef)
   {
     $("#alertDiv").show();
-	  jQuery.i18n.prop('login_error_username');
+    jQuery.i18n.prop('login_error_username');
     $("#alertMessage").html(login_error_username);
     $("#ajaxLoader").hide();
     return false;
@@ -53,210 +50,195 @@ function loginHandler()
   if (pass == '' || pass == passDef)
   {
     $("#alertDiv").show();
-	  jQuery.i18n.prop('login_error_password');
+    jQuery.i18n.prop('login_error_password');
     $("#alertMessage").html(login_error_password);
     $("#ajaxLoader").hide();
     return false;
   }
-  loginAsk (user.toLowerCase(), pass, r);
+  loginAsk(user.toLowerCase(), pass, r);
 }
-
-	
 // new version
 function loginAs(type)
 {
-	var user, pass=MD5('askask');
-	switch (type)
-	{
-		case 'planner':
-			user = 'beheer';
-			pass = '319fcaa585c17eff5dcc2a57e8fc853f';
-		break;
-		case 'teamleider':
-			user = '4058fok';
-			pass = '288116504f5e303e4be4ff1765b81f5d';
-		break;
-		case 'volunteer':
-			user = '4780aldewereld';
-			pass = 'd9a6c9bad827746190792cf6f30d5271';
-		break;		
-	}
-	loginAskWithOutMD5 (user, pass, true);
+  var user, pass = MD5('askask');
+  switch (type)
+  {
+    case 'planner':
+      user = 'beheer';
+      pass = '319fcaa585c17eff5dcc2a57e8fc853f';
+      break;
+    case 'teamleider':
+      user = '4058fok';
+      pass = '288116504f5e303e4be4ff1765b81f5d';
+      break;
+    case 'volunteer':
+      user = '4780aldewereld';
+      pass = 'd9a6c9bad827746190792cf6f30d5271';
+      break;
+  }
+  loginAskWithOutMD5(user, pass, true);
 }
 
-
-function loginAsk (user, pass, r)
+function loginAsk(user, pass, r)
 {
-	webpaige.set('config', '{}');
-	
-	// logging in ask
-	webpaige.con(
-		options = {
-			path: '/login?uuid=' + user + '&pass=' + MD5(pass),
-			//loading: 'Logging in..'
-			loading: 'Inloggen..'
-		},
-		function(data, label)
-	  {  	
-      if (r != null)
+  webpaige.set('config', '{}');
+  // logging in ask
+  webpaige.con(
+  options = {
+    path: '/login?uuid=' + user + '&pass=' + MD5(pass),
+    //loading: 'Logging in..'
+    loading: 'Inloggen..'
+  },
+
+  function (data, label)
+  {
+    if (r != null)
+    {
+      var login = {};
+      login.user = user;
+      login.remember = r;
+      webpaige.set('login', JSON.stringify(login));
+    }
+    else
+    {
+      webpaige.remove('login');
+    }
+    session.setSession(data["X-SESSION_ID"]);
+    document.cookie = "sessionId=" + session;
+    // loading resources
+    webpaige.con(
+    options = {
+      path: '/resources',
+      //loading: 'Loading resources..',
+      loading: 'Persoonlijke gegevens aan het laden..',
+      label: 'resources',
+      session: session.getSession()
+    },
+
+    function (data, label)
+    {
+      webpaige.set(label, JSON.stringify(data));
+      webpaige.config('userRole', data.role);
+      var trange = {};
+      now = parseInt((new Date()).getTime() / 1000);
+      trange.bstart = (now - 86400 * 7 * 1);
+      trange.bend = (now + 86400 * 7 * 1);
+      trange.start = new Date();
+      trange.start = Date.today().add(
       {
-      	var login = {};
-      	login.user = user;
-      	login.remember = r;
-      	webpaige.set('login', JSON.stringify(login));
-      }
-      else
+        days: -5
+      });
+      trange.end = new Date();
+      trange.end = Date.today().add(
       {
-      	webpaige.remove('login');
-      }
-      session.setSession(data["X-SESSION_ID"]);
-      document.cookie = "sessionId=" + session;
-      
-      // loading resources
-			webpaige.con(
-				options = {
-					path: '/resources',
-					//loading: 'Loading resources..',
-					loading: 'Persoonlijke gegevens aan het laden..',
-					label: 'resources',
-					session: session.getSession()	
-				},
-				function(data, label)
-			  {  	
-					webpaige.set(label, JSON.stringify(data));
-					
-					webpaige.config('userRole', data.role);					
-					
-					var trange = {};	
-					
-				  now = parseInt((new Date()).getTime() / 1000);
-				  
-				  trange.bstart = (now - 86400 * 7 * 1);
-				  trange.bend = (now + 86400 * 7 * 1);					
-					
-				  trange.start = new Date();
-				  trange.start = Date.today().add({ days: -5 });
-				  trange.end = new Date();
-				  trange.end = Date.today().add({ days: 5 });
-				  
-				  webpaige.config('trange', trange);	
-				  webpaige.config('treset', trange);		
-							
-		      var url = (data.role < 3) ? '/network' : '/parent';
-					
-					webpaige.con(
-						options = {
-							path: url,
-							loading: 'Groep & contacten informatie aan het laden..',
-							label: 'groups'
-							,session: session.getSession()	
-						},
-						function(data, label)
-					  { 
-						  for(var i in data)
-						  {
-						  	if (i == 0)
-						  	{
-						  		webpaige.config('firstGroupUUID', data[i].uuid);
-						  		webpaige.config('firstGroupName', data[i].name);
-						  	}	 	    
-						  }
-				      // finally redirect
-							document.location = "dashboard.html";
-						}
-					);
- 	
-				}
-			);
-		}
-	);
+        days: 5
+      });
+      webpaige.config('trange', trange);
+      webpaige.config('treset', trange);
+      var url = (data.role < 3) ? '/network' : '/parent';
+      webpaige.con(
+      options = {
+        path: url,
+        loading: 'Groep & contacten informatie aan het laden..',
+        label: 'groups',
+        session: session.getSession()
+      },
+
+      function (data, label)
+      {
+        for (var i in data)
+        {
+          if (i == 0)
+          {
+            webpaige.config('firstGroupUUID', data[i].uuid);
+            webpaige.config('firstGroupName', data[i].name);
+          }
+        }
+        // finally redirect
+        document.location = "dashboard.html";
+      });
+    });
+  });
 }
 
-function loginAskWithOutMD5 (user, pass, r)
+function loginAskWithOutMD5(user, pass, r)
 {
-	webpaige.set('config', '{}');
-	
-	// logging in ask
-	webpaige.con(
-		options = {
-			path: '/login?uuid=' + user + '&pass=' + pass,
-			label: 'login',
-			loading: 'Inloggen..'
-		},
-		function(data, label)
-	  {  	
-      if (r != null)
+  webpaige.set('config', '{}');
+  // logging in ask
+  webpaige.con(
+  options = {
+    path: '/login?uuid=' + user + '&pass=' + pass,
+    label: 'login',
+    loading: 'Inloggen..'
+  },
+
+  function (data, label)
+  {
+    if (r != null)
+    {
+      var login = {};
+      login.user = user;
+      login.remember = r;
+      webpaige.set('login', JSON.stringify(login));
+    }
+    else
+    {
+      webpaige.remove('login');
+    }
+    session.setSession(data["X-SESSION_ID"]);
+    document.cookie = "sessionId=" + session;
+    // loading resources
+    webpaige.con(
+    options = {
+      path: '/resources',
+      loading: 'Persoonlijke gegevens aan het laden..',
+      label: 'resources',
+      session: session.getSession()
+    },
+
+    function (data, label)
+    {
+      webpaige.set(label, JSON.stringify(data));
+      webpaige.config('userRole', data.role);
+      var trange = {};
+      now = parseInt((new Date()).getTime() / 1000);
+      trange.bstart = (now - 86400 * 7 * 1);
+      trange.bend = (now + 86400 * 7 * 1);
+      trange.start = new Date();
+      trange.start = Date.today().add(
       {
-      	var login = {};
-      	login.user = user;
-      	login.remember = r;
-      	webpaige.set('login', JSON.stringify(login));
-      }
-      else
+        days: -5
+      });
+      trange.end = new Date();
+      trange.end = Date.today().add(
       {
-      	webpaige.remove('login');
-      }
-      session.setSession(data["X-SESSION_ID"]);
-      document.cookie = "sessionId=" + session;
-      
-      // loading resources
-			webpaige.con(
-				options = {
-					path: '/resources',
-					loading: 'Persoonlijke gegevens aan het laden..',
-					label: 'resources',
-					session: session.getSession()	
-				},
-				function(data, label)
-			  {  	
-					webpaige.set(label, JSON.stringify(data));
-					
-					webpaige.config('userRole', data.role);			
-					
-					var trange = {};	
-					
-				  now = parseInt((new Date()).getTime() / 1000);
-				  
-				  trange.bstart = (now - 86400 * 7 * 1);
-				  trange.bend = (now + 86400 * 7 * 1);					
-					
-				  trange.start = new Date();
-				  trange.start = Date.today().add({ days: -5 });
-				  trange.end = new Date();
-				  trange.end = Date.today().add({ days: 5 });
-				  
-				  webpaige.config('trange', trange);	
-				  webpaige.config('treset', trange);		
-				      
-		      var url = (data.role < 3) ? '/network' : '/parent';				      				      
-		      
-					//debugger;
-					
-					webpaige.con(
-						options = {
-							path: url,
-							loading: 'Groep & contacten informatie aan het laden..',
-							label: 'groups'
-							,session: session.getSession()	
-						},
-						function(data, label)
-					  { 
-						  for(var i in data)
-						  {
-						  	if (i == 0)
-						  	{
-						  		webpaige.config('firstGroupUUID', data[i].uuid);
-						  		webpaige.config('firstGroupName', data[i].name);
-						  	}	 	    
-						  }
-						  						  
-				      // finally redirect
-							document.location = "dashboard.html";
-						}
-					);
- 	
-				}
-			);
-		}
-	);
+        days: 5
+      });
+      webpaige.config('trange', trange);
+      webpaige.config('treset', trange);
+      var url = (data.role < 3) ? '/network' : '/parent';
+      //debugger;
+      webpaige.con(
+      options = {
+        path: url,
+        loading: 'Groep & contacten informatie aan het laden..',
+        label: 'groups',
+        session: session.getSession()
+      },
+
+      function (data, label)
+      {
+        for (var i in data)
+        {
+          if (i == 0)
+          {
+            webpaige.config('firstGroupUUID', data[i].uuid);
+            webpaige.config('firstGroupName', data[i].name);
+          }
+        }
+        // finally redirect
+        document.location = "dashboard.html";
+      });
+    });
+  });
 }
--- war/js/views/settings.js
+++ war/js/views/settings.js
@@ -1,88 +1,62 @@
 
-window.addEventListener( 'load', windowInit, false );
+window.addEventListener('load', windowInit, false);
 
 function windowInit()
 {
- 	pageInit('settings', 'true');	
-
- 	renderGroupsList();
-  
+  pageInit('settings', 'true');
+  renderGroupsList();
   $('#from').datetimepicker();
   $('#till').datetimepicker();
   $('#efrom').datetimepicker();
   $('#etill').datetimepicker();
-  
-  wishesTimelineInit(); 
-
-		var local = {
-			title: 'settings_title',
-			statics: ['settings_today', 'settings_new_wish', 'settings_group', 'settings_from', 'settings_till', 'settings_wish', 'settings_cancel', 'settings_save_wish', 'settings_edit_wish', 'settings_settings']		
-		}
-		webpaige.i18n(local); 
- 	
+  wishesTimelineInit();
+  var local = {
+    title: 'settings_title',
+    statics: ['settings_today', 'settings_new_wish', 'settings_group', 'settings_from', 'settings_till', 'settings_wish', 'settings_cancel', 'settings_save_wish', 'settings_edit_wish', 'settings_settings']
+  }
+  webpaige.i18n(local);
 }
-    
-
-	
 var session = new ask.session();
 var timeline;
 var timeline_selected = null;
 
-
 function wishSubmit()
 {
   $('#newWish').modal('hide');
-  
   var group = $('#groupsList option:selected').val();
-  
   var from = $('#from').val();
   var till = $('#till').val();
-  
   var wish = $('#wish').val();
-  
   var from = Date.parse(from, "dd-MMM-yyyy HH:mm");
   var from = from.getTime();
-  
   var till = Date.parse(till, "dd-MMM-yyyy HH:mm");
   var till = till.getTime();
-  
   addWish(from, till, wish, group);
-  
   $('#newWishForm')[0].reset();
 }
-
-
 
 function editPlanSubmit()
 {
   $('#editEvent').modal('hide');
-  
   var enewType = $('input#eplanningType[name="eplanningType"]:checked').val();
   var enewFrom = $('#eplanningFrom').val();
   var enewTill = $('#eplanningTill').val();
   var enewReoccuring = $('input#eplanningReoccuring:checkbox:checked').val();
-  
-  if (enewReoccuring == "true") enewReoccuring = "true"; else enewReoccuring = "false";
-  
+  if (enewReoccuring == "true") enewReoccuring = "true";
+  else enewReoccuring = "false";
   var enewFrom = Date.parse(enewFrom, "dd-MMM-yyyy HH:mm");
   var enewFrom = enewFrom.getTime();
-  
   var enewTill = Date.parse(enewTill, "dd-MMM-yyyy HH:mm");
   var enewTill = enewTill.getTime();
-  
-	window.newslot = {
-		from:	Math.round(enewFrom/1000),
-		till: Math.round(enewTill/1000),
-		reoc: enewReoccuring,
-		type: enewType
-	};
-
-	updateSlotModal();
-	
+  window.newslot = {
+    from: Math.round(enewFrom / 1000),
+    till: Math.round(enewTill / 1000),
+    reoc: enewReoccuring,
+    type: enewType
+  };
+  updateSlotModal();
   $('#editEventForm')[0].reset();
 }
-
-
 
 function deletePlanSubmit()
 {
@@ -91,14 +65,10 @@ function deletePlanSubmit()
   $('#editEventForm')[0].reset();
 }
 
-
-
-
-
 function editWishModal(start, end, group, wish)
 {
   $('#editWish').modal('show');
-  $('#editWish span#groupName').html(group);  
+  $('#editWish span#groupName').html(group);
   start = new Date(start.getTime());
   start = start.toString("dd-MMM-yyyy HH:mm");
   $('#editWish #efrom').val(start);
@@ -108,156 +78,113 @@ function editWishModal(start, end, group, wish)
   $('#editWish #ewish').val(wish);
 }
 
-
-
 function editWishSubmit()
 {
   $('#editWish').modal('hide');
-  var group = $('#editWish span#groupName').html();  
+  var group = $('#editWish span#groupName').html();
   var start = $('#editWish #efrom').val();
   var end = $('#editWish #etill').val();
   var wish = $('#editWish #ewish').val();
-  
   start = Date.parse(start, "dd-MMM-yyyy HH:mm");
   start = start.getTime();
-  
   end = Date.parse(end, "dd-MMM-yyyy HH:mm");
   end = end.getTime();
-  
-	var groups = JSON.parse(webpaige.get('groups'));
-  
+  var groups = JSON.parse(webpaige.get('groups'));
   for (var i in groups)
   {
-	  if (groups[i].name == group)
-	  {
-		  var guuid = groups[i].uuid;
-	  }	  
+    if (groups[i].name == group)
+    {
+      var guuid = groups[i].uuid;
+    }
   }
-  
   addWish(start, end, wish, guuid);
 }
 
-
-
 function addWish(from, till, wish, group)
 {
-  var body = 	'{"end":' + (till / 1000) + 
-  						',"start":' + (from / 1000) + 
-  						',"wish":"' + wish + '"}';
-	webpaige.con(
-		options = {
-			type: 'put',
-			path: '/network/'+group+'/wish',
-			json: body,
-			loading: 'Nieuwe beschiekbaarheid wordt toegevoegd..'
-			,session: session.getSession()	
-		},
-		function(data)
-	  {  
-			getWishes();
-		}
-	); 
-} 
+  var body = '{"end":' + (till / 1000) + ',"start":' + (from / 1000) + ',"wish":"' + wish + '"}';
+  webpaige.con(
+  options = {
+    type: 'put',
+    path: '/network/' + group + '/wish',
+    json: body,
+    loading: 'Nieuwe beschiekbaarheid wordt toegevoegd..',
+    session: session.getSession()
+  },
 
-
-
+  function (data)
+  {
+    getWishes();
+  });
+}
 // Timneline initators
 function wishesTimelineInit()
 {
   timeline_data = [];
-  var options = { 
-  };
+  var options = {};
   timeline = new links.Timeline(document.getElementById('wishesTimeline'));
-  
   // Add event listeners
-  google.visualization.events.addListener(timeline, 'edit', 	timelineOnEdit);
-  google.visualization.events.addListener(timeline, 'add', 		timelineOnAdd);
+  google.visualization.events.addListener(timeline, 'edit', timelineOnEdit);
+  google.visualization.events.addListener(timeline, 'add', timelineOnAdd);
   google.visualization.events.addListener(timeline, 'delete', timelineOnDelete);
   google.visualization.events.addListener(timeline, 'change', timelineOnChange);
   google.visualization.events.addListener(timeline, 'select', timelineOnSelect);
-  
   getWishes();
 }
 
 function getWishes()
 {
-	
   var now = parseInt((new Date()).getTime() / 1000);
-  var range =	'start=' + (now - 86400 * 7 * 4 * 4) + 
-  						'&end=' + (now + 86400 * 7 * 4 * 4);  
-	
-	timeline_data = new google.visualization.DataTable();
-	timeline_data.addColumn('datetime', 'start');
-	timeline_data.addColumn('datetime', 'end');
-	timeline_data.addColumn('string', 'content');
-	timeline_data.addColumn('string', 'group');
-						 						
-	webpaige.con(
-		options = {
-			path: '/network',
-			loading: 'Groepen worden opgeladen..',
-			label: 'groups'
-			,session: session.getSession()	
-		},
-		function(data, label)
-	  { 		
-		  for(var i in data)
-		  { 						
-				webpaige.con(
-					options = {
-						path: '/network/'+data[i].uuid+'/wish?'+range,
-						loading: data[i].name+' beschiekbaarheid wordt opgeladen..',
-						label: data[i].name
-						,session: session.getSession()	
-					},
-					function(data, label)
-				  {
-						for (var i in data)
-						{
-						  var content = '<div class="wishslot c-' + data[i].count + '">' + data[i].count + '</div>';
-						  timeline_data.addRow([
-						  	new Date(data[i].start * 1000), 
-						  	new Date(data[i].end * 1000), 
-						  	content, 
-						  	label
-						  ]
-						  );
-						}        
-					  var options = {
-					      'selectable': true,
-					      'editable': true,
-					      'groupsWidth': '100px'
-					  };
-						timeline.draw(timeline_data, options); 
-					}
-				); 
-		  }
-		}
-	);
-	
-	
+  var range = 'start=' + (now - 86400 * 7 * 4 * 4) + '&end=' + (now + 86400 * 7 * 4 * 4);
+  timeline_data = new google.visualization.DataTable();
+  timeline_data.addColumn('datetime', 'start');
+  timeline_data.addColumn('datetime', 'end');
+  timeline_data.addColumn('string', 'content');
+  timeline_data.addColumn('string', 'group');
+  webpaige.con(
+  options = {
+    path: '/network',
+    loading: 'Groepen worden opgeladen..',
+    label: 'groups',
+    session: session.getSession()
+  },
+
+  function (data, label)
+  {
+    for (var i in data)
+    {
+      webpaige.con(
+      options = {
+        path: '/network/' + data[i].uuid + '/wish?' + range,
+        loading: data[i].name + ' beschiekbaarheid wordt opgeladen..',
+        label: data[i].name,
+        session: session.getSession()
+      },
+
+      function (data, label)
+      {
+        for (var i in data)
+        {
+          var content = '<div class="wishslot c-' + data[i].count + '">' + data[i].count + '</div>';
+          timeline_data.addRow([
+          new Date(data[i].start * 1000),
+          new Date(data[i].end * 1000),
+          content,
+          label]);
+        }
+        var options = {
+          'selectable': true,
+          'editable': true,
+          'groupsWidth': '100px'
+        };
+        timeline.draw(timeline_data, options);
+      });
+    }
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Timeline events
 function timelineOnAdd()
-{
-}
+{}
 
 function timelineOnEdit()
 {
@@ -265,13 +192,12 @@ function timelineOnEdit()
   var row = sel[0].row;
   var item = timeline.getItem(row);
   var wish = timeline_data.getValue(row, 2).split('>')[1].split('<')[0];
-  
   editWishModal(item.start, item.end, item.group, wish);
 }
 
 function timelineOnDelete()
 {
-	timeline.cancelChange();
+  timeline.cancelChange();
 }
 
 function timelineOnSelect()
@@ -283,23 +209,8 @@ function timelineOnSelect()
 
 function timelineOnChange()
 {
-	timeline.cancelChange();
+  timeline.cancelChange();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Timeline navigations
 function timelineZoomIn()
 {
@@ -336,26 +247,23 @@ function timelineMoveRight()
   timeline.trigger("rangechange");
   timeline.trigger("rangechanged");
 }
-
-
-
 // Group list producers
 function renderGroupsList()
 {
-	webpaige.con(
-		options = {
-			path: '/network',
-			loading: 'Loading groups..',
-			label: 'groups'
-			,session: session.getSession()	
-		},
-		function(data, label)
-	  {
-	  	webpaige.set(label, JSON.stringify(data)); 
-		  for(var i in data)
-		  {  
-		  	$('#groupsList').append('<option value="'+data[i].uuid+'">'+data[i].name+'</option>');		    
-		  }
-		}
-	);
+  webpaige.con(
+  options = {
+    path: '/network',
+    loading: 'Loading groups..',
+    label: 'groups',
+    session: session.getSession()
+  },
+
+  function (data, label)
+  {
+    webpaige.set(label, JSON.stringify(data));
+    for (var i in data)
+    {
+      $('#groupsList').append('<option value="' + data[i].uuid + '">' + data[i].name + '</option>');
+    }
+  });
 }
